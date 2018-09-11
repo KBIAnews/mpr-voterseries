4e6c757cf2d19bc13faf7ee6f4d25c2df0361066
Nathan Lawrence <nathan.Lawrence.barrick@gmail.com>
2018-09-11 14:54:24 -0500

    Add sass support.

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,9 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby Default Starter',
+    title: 'Beyond the Ballot',
   },
-  plugins: ['gatsby-plugin-react-helmet'],
+  plugins: [
+    'gatsby-plugin-react-helmet',
+    'gatsby-plugin-sass'
+  ],
 }
