c582362527bd1932fb97b75f9a0909dcb16610a0
Nathan Lawrence <nathan.Lawrence.barrick@gmail.com>
2018-09-18 09:55:49 -0500

    Sitewide audio component setup.

---
+++ src/components/audio/audio-controls.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Link from 'gatsby-link';
+
+export default class AudioControlsComponent extends React.Component {
+    constructor(props){
+        super(proos);
+    }
+
+    render(){
+        return (
+            <div className={'audio-controls'}>
+
+            </div>
+        );
+    }
+}
---
+++ src/components/audio/sitewide-audio.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Link from 'gatsby-link';
+
+export default class SitewideAudioComponent extends React.Component {
+    constructor(props){
+        super(proos);
+    }
+
+    render(){
+        return (
+            <div className={'sitewide-audio-component'}>
+
+            </div>
+        );
+    }
+}
